d45eef68817f56747dee64bf12f6c8ec2d735816
[STV]Kazunori Aoki <k.aoki@stv-tech.co.jp>
2021-03-24 10:05:14 +0900

    add widget test

--- unit_test_sample/test/widget_test.dart
+++ unit_test_sample/test/widget_test.dart
@@ -10,4 +10,72 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:unit_test_sample/main.dart';
 
-void main() {}
+void main() {
+  group('Find by Type', () {
+    testWidgets('Test to see that Material App widget is in Tree',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
+
+      expect(find.byType(MaterialApp), findsOneWidget);
+    });
+
+    testWidgets('Test to see that CircularProgressIndicator widget is in tree',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: MyHomePage(),
+        ),
+      );
+
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    });
+  });
+
+  group('Applcation Logic', () {
+    testWidgets('Add a todo', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: MyHomePage()));
+
+      final textFinder = find.byKey(Key('todo-field'));
+      final addButtonFinder = find.byKey(Key('add-todo'));
+      final removeTodoFinder = find.byKey(Key('gd-0'));
+
+      // add test
+      await tester.enterText(textFinder, 'A sample todo');
+      await tester.tap(addButtonFinder);
+      await tester.pump();
+      expect(find.text('A sample todo'), findsOneWidget);
+
+      // remove test
+      await tester.longPress(removeTodoFinder);
+      await tester.pump();
+      // 指定された期間繰り返す
+      // tester.pumpAndSettle();
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      expect(find.byType(CheckboxListTile), findsNothing);
+    });
+
+    testWidgets('FInish a todo', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: MyHomePage()));
+
+      final textFinder = find.byKey(Key('todo-field'));
+      final addButtonFinder = find.byKey(Key('add-todo'));
+      final todoFinder = find.byKey(ValueKey('0-false-checkbox'));
+
+      await tester.enterText(textFinder, 'A sample todo');
+      await tester.tap(addButtonFinder);
+      await tester.pump();
+
+      await tester.enterText(textFinder, 'Another todo');
+      await tester.tap(addButtonFinder);
+      await tester.pump();
+
+      await tester.tap(todoFinder);
+      await tester.pump();
+
+      expect(find.text('A sample todo'), findsOneWidget);
+      expect(find.text('Another todo'), findsOneWidget);
+      expect(find.byKey(ValueKey('0-true-checkbox')), findsOneWidget);
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+    });
+  });
+}
